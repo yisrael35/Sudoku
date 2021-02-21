9d6aa40ffafcb68e338633069e31ab45cf331dac
yisrael bar <yisrael35@gmail.com>
2021-02-21 19:14:06 +0200

    new expo

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import MyComponent from "./app/screens/table";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-//   return <WelcomeScreen />;
-  return <App />;
+  return (
+    <View style={styles.container}>
+      <Text>razi and yisrael makes sudoku</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
