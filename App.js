81d51e3d255214debe9b2c58b8358f75a740479b
yisrael35 <yisrael35@gmail.com>
2021-03-13 15:56:58 +0200

    check

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff1f',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
